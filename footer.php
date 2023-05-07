0310b3f50acca9949f72b687a6f9f34c6cf2ebdf
wwlinne <87803550+wwlinne@users.noreply.github.com>
2023-05-07 10:39:34 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer>
-        <p>&copy; <?php echo date("Y");?>  CST8285.
+        <p>&copy; <?php echo date("Y");?>  Web Programming.
         <a href = "https://www.tutorialrepublic.com/php-tutorial/php-mysql-crud-application.php">
             Updated educational version inspired from tutorialrepublic CRUD sample
             </a>
